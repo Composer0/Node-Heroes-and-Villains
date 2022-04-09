ad5e842f68b3c59633502ba5d4f675e09ea4bb14
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-04-09 12:09:17 -0400

    Added index.js for Node practice.

---
+++ index.js
@@ -0,0 +1,8 @@
+var superVillains = require('supervillains');
+var superHeroes = require('superheroes');
+
+var myVillainName = superVillains.random();
+var myHeroName = superHeroes.random();
+
+console.log("My Villain name is " + myVillainName);
+console.log("My Hero name is " + myHeroName);
