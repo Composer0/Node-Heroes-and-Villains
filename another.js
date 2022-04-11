87561020dce96f673b09d37c588291d697d7078e
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-04-10 23:10:27 -0400

    Pokemon addon for practice

---
+++ another.js
@@ -0,0 +1,14 @@
+const pokemon = require('pokemon');
+
+pokemon.all();
+
+pokemon.random();
+
+pokemon.getName(27);
+
+pokemon.getId('Dratini');
+
+console.log(pokemon.getName(27));
+console.log(pokemon.all());
+console.log(pokemon.random());
+console.log(pokemon.getId('Dratini'));
