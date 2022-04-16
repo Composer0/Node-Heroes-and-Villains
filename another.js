43c1a67208d0c3f9ddcca5fad020543a20c915ab
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-04-15 23:25:22 -0400

    Update
    Correct values

--- another.js
+++ another.js
@@ -4,11 +4,11 @@ pokemon.all();
 
 pokemon.random();
 
-pokemon.getName(28);
+pokemon.getName(27);
 
 pokemon.getId('Mewtwo');
 
 console.log(pokemon.getName(27));
 console.log(pokemon.all());
 console.log(pokemon.random());
-console.log(pokemon.getId('Dratini'));
+console.log(pokemon.getId('Mewtwo'));
